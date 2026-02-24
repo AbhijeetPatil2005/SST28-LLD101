0769eff429645c426be2b1ad4254be5f3d86d351
Abhijeet Patil <abhijeetpatil032005@gmail.com>
2026-02-24 14:14:14 +0530

    commented

--- SOLID/Ex3/src/EligibilityEngine.java
+++ SOLID/Ex3/src/EligibilityEngine.java
@@ -40,6 +40,7 @@ public class EligibilityEngine {
 class EligibilityEngineResult {
     public final String status;
     public final List<String> reasons;
+
     public EligibilityEngineResult(String status, List<String> reasons) {
         this.status = status;
         this.reasons = reasons;
